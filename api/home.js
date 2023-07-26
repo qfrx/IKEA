16e0b3411508343f508caaffba4c7cb45ef83723
qfrx <1817721918@qq.com>
2023-07-26 13:52:07 +0800

    完善api封装，添加api传参方法，首页进度更新

--- api/home.js
+++ api/home.js
@@ -28,11 +28,30 @@ export function getRecommendationsCard(data) {
 		data,
 	});
 }
-// 
+//
 // 简易版热销排行数据
 export function getEankingEasy(data) {
 	return request({
 		url: '/ranking/easy',
+		method: 'GET',
+		data,
+	});
+}
+
+
+// 简易版热销排行数据
+export function getRoomExplore(data) {
+	return request({
+		url: '/roomExplore',
+		method: 'GET',
+		data,
+	});
+}
+
+// 发现更多灵感  参数：type：分类，pageNum：页数
+export function getdiscoveringInspiration(data) {
+	return request({
+		url: '/discoveringInspiration',
 		method: 'GET',
 		data,
 	});
